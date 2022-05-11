7152e2f840173b2e33be43bf69e3c1c4304dd51a
Haswell33 <31355807+Haswell33@users.noreply.github.com>
2022-05-11 12:20:33 +0200

    Update closePurchase.js

--- closePurchase/closePurchase.js
+++ closePurchase/closePurchase.js
@@ -2,7 +2,7 @@
 	$(function() {
     	AJS.dialog2.on('show', function(event) {
         	if (event.target.id === 'close-purchase-dialog') {
-            	const expireTimeField = document.getElementById('expire-time-field')
+                const expireTimeField = $(event.target).find('#expire-time-field')[0]
                 const controller = new AJS.DatePicker(expireTimeField, {'overrideBrowserDefault': true})
                 var softwareOptions = []
                 AJS.$.ajax({
@@ -25,7 +25,17 @@
                             text: term + ' (new)',
                             newTag: true // add additional parameters
                         }
-                    }
+                    },
+                    /*createTag: function (params, term) {
+                                    	return {
+                                        	id: params.term + ' new',
+                                        	text: params.term + ' new',
+                                        	newOption: true
+                                        }
+                                    }
+                             formatNoMatches: function(term) {
+        								return '<strong>' + term + '</strong> (New Component)';
+    						}*/
                 });
             	$(event.target).find("#close-button").click(function(e) {
                 	e.preventDefault();
@@ -38,30 +48,30 @@
                 	AJS.dialog2(event.target).remove();
               	});
               	$(event.target).find('#create-asset-toggle').change(function() {
-                	let toggleIsChecked = $('#create-asset-toggle').prop('checked')
-                    globalFields(toggleIsChecked)
+                	let toggleIsChecked = $(event.target).find('#create-asset-toggle').prop('checked')
+                    globalFields(event, toggleIsChecked)
                   	if (toggleIsChecked) {
-                        deviceFields('show')
+                        deviceFields(event, 'show')
                         $(event.target).find('#asset-type-field-group').change(function() {
-                        	if ($('#license-radio-field').prop('checked')) {
-                                licenseFields('show')
-                                deviceFields('hide')
+                        	if ($(event.target).find('#license-radio-field').prop('checked')) {
+                                licenseFields(event, 'show')
+                                deviceFields(event, 'hide')
                     		}
-                     		else if ($('#device-radio-field').prop('checked')) {
-                                licenseFields('hide')
-                                deviceFields('show')
+                     		else if ($(event.target).find('#device-radio-field').prop('checked')) {
+                                licenseFields(event, 'hide')
+                                deviceFields(event, 'show')
                             }
                         })
                     } 
                     else {
-                        deviceFields('hide')
-                      	licenseFields('hide')
+                        deviceFields(event, 'hide')
+                      	licenseFields(event, 'hide')
                     }
                   }); 
                   $(event.target).find("#create-button").click(function(e) {
                   		let toggleIsChecked = $('#create-asset-toggle').prop('checked');
                         var requiredFieldsFilled = true
-                        $('#close-purchase-form input').each(function (){
+                        $(event.target).find('#close-purchase-form input').each(function (){
                         	if ($(this).is(':visible') && this.value === '' && !(this.id).contains('s2id_autogen')){ 
                             	requiredFieldsFilled = false  
                                 console.error(this.id + ' is empty')
@@ -75,7 +85,7 @@
                         	});
                         }
                   		if (toggleIsChecked && requiredFieldsFilled) {
-                        	var softwareFieldData = $('#software-field').select2('data')
+                        	var softwareFieldData = $(event.target).find('#software-field').select2('data')
                             var softwareFieldChoices = []
                         	softwareFieldData.forEach(function(elem){
                    				softwareFieldChoices.push(elem.text)
@@ -83,15 +93,15 @@
                             AJS.$.ajax({
                                 url: "/rest/scriptrunner/latest/custom/closePurchase" + 
                                     '?createAsset=' 	+ toggleIsChecked + 
-                                    '&summary=' 		+ $('#summary-field').val() + 
-                                    '&assetType='		+ $('input[name=asset-type-radio]').filter(":checked").val() +
-                                    '&serviceTag=' 	  	+ $('#service-tag-field').val() + 
-                                    '&model=' 		  	+ $('#model-field').val() + 
-                                    '&invoiceNumber=' 	+ $('#invoice-number-field').val() + 
+                                    '&summary=' 		+ $(event.target).find('#summary-field').val() + 
+                                    '&assetType='		+ $(event.target).find('input[name=asset-type-radio]').filter(":checked").val() +
+                                    '&serviceTag=' 	  	+ $(event.target).find('#service-tag-field').val() + 
+                                    '&model=' 		  	+ $(event.target).find('#model-field').val() + 
+                                    '&invoiceNumber=' 	+ $(event.target).find('#invoice-number-field').val() + 
                                     '&software='		+ softwareFieldChoices.join(',') +
-                                    '&licenseType='		+ $('#license-type-field').val() +
-                                    '&expireTime='		+ $('#expire-time-field').val() +
-                                    '&description='		+ $('#description-field').val() +
+                                    '&licenseType='		+ $(event.target).find('#license-type-field').val() +
+                                    '&expireTime='		+ $(event.target).find('#expire-time-field').val() +
+                                    '&description='		+ $(event.target).find('#description-field').val() +
                                     '&issueKey=' 	  	+ JIRA.Issue.getIssueKey(),
                                 type: 'POST',
                                 dataType: 'json',
@@ -123,7 +133,7 @@
                             AJS.$.ajax({
                                 url: "/rest/scriptrunner/latest/custom/closePurchase" + 
                                     "?createAsset=" 	+ toggleIsChecked + 
-                                    "&cost=" 			+ $("#cost-field").val() + 
+                                    "&cost=" 			+ $(event.target).find("#cost-field").val() + 
                                     "&issueKey=" 		+ JIRA.Issue.getIssueKey(),
                                 type: 'POST',
                                 dataType: 'json',
@@ -153,68 +163,68 @@
             });
         });
         
-        function deviceFields (oper){
+        function deviceFields (event, oper){
         	switch(oper){
         		case 'hide':
-                	$('#service-tag-field-group').hide()
-                    $('#place-field-group').hide()
-                    $('#model-field-group').hide()
-                    $('#invoice-number-field-group').hide()
+                	$(event.target).find('#service-tag-field-group').hide()
+                    $(event.target).find('#place-field-group').hide()
+                    $(event.target).find('#model-field-group').hide()
+                    $(event.target).find('#invoice-number-field-group').hide()
                     break
                 case 'show':
-                	$('#service-tag-field-group').show()
-                    $('#place-field-group').show()
-                    $('#model-field-group').show()
-                    $('#invoice-number-field-group').show()
+                	$(event.target).find('#service-tag-field-group').show()
+                    $(event.target).find('#place-field-group').show()
+                    $(event.target).find('#model-field-group').show()
+                    $(event.target).find('#invoice-number-field-group').show()
                     break
                 default:
-                	log.error('not found oper')
+                	console.error('not found oper')
         	}
         }
         
-        function licenseFields (oper){
+        function licenseFields (event, oper){
         	switch (oper){
             	case 'hide':
-                	$('#software-field-group').hide()
-                    $('#license-type-field-group').hide()
-                    $('#expire-time-field-group').hide()
+                	$(event.target).find('#software-field-group').hide()
+                    $(event.target).find('#license-type-field-group').hide()
+                    $(event.target).find('#expire-time-field-group').hide()
                 	break
                 case 'show':
-                    $('#software-field-group').show()
-                    $('#license-type-field-group').show()
-                    $('#license-type-field').change(function() {
-                        if ($('#license-type-field').val() === 'Subscription')
-                            $('#expire-time-field-group').show()
+                    $(event.target).find('#software-field-group').show()
+                    $(event.target).find('#license-type-field-group').show()
+                    $(event.target).find('#license-type-field').change(function() {
+                        if ($(event.target).find('#license-type-field').val() === 'Subscription')
+                            $(event.target).find('#expire-time-field-group').show()
                         else 
-                            $('#expire-time-field-group').hide() 
+                            $(event.target).find('#expire-time-field-group').hide() 
                     })
                     break
                 default:
-                	log.error('not found oper')
+                	console.error('not found oper')
             }
         }
         
-        function globalFields(toggleIsChecked){
+        function globalFields(event, toggleIsChecked){
         	switch (toggleIsChecked){
             	case false:
-                	$('#cost-field-group').show()
-                    $('#not-create-asset-paragraph').show() 
-                	$('#asset-type-field-group').hide()
-                    $('#summary-field-group').hide()
-                    $('#description-field-group').hide()
-                    $('#create-asset-paragraph').hide()
+                	$(event.target).find('#cost-field-group').show()
+                    $(event.target).find('#not-create-asset-paragraph').show() 
+                	$(event.target).find('#asset-type-field-group').hide()
+                    $(event.target).find('#summary-field-group').hide()
+                    $(event.target).find('#description-field-group').hide()
+                    $(event.target).find('#create-asset-paragraph').hide()
                 	break
                 case true:
-                    $('#cost-field-group').hide()
-                    $('#not-create-asset-paragraph').hide() 
-                	$('#asset-type-field-group').show()
-                    $('#summary-field-group').show()
-                    $('#description-field-group').show()
-                    $('#create-asset-paragraph').show()
-					$("#device-radio-button").prop("checked", true);
+                    $(event.target).find('#cost-field-group').hide()
+                    $(event.target).find('#not-create-asset-paragraph').hide() 
+                	$(event.target).find('#asset-type-field-group').show()
+                    $(event.target).find('#summary-field-group').show()
+                    $(event.target).find('#description-field-group').show()
+                    $(event.target).find('#create-asset-paragraph').show()
+					$(event.target).find("#device-radio-button").prop("checked", true);
                     break
                 default:
-                	log.error('provided value should be boolean')
+                	console.error('provided value should be boolean')
             }
         }
 })(AJS.$);
