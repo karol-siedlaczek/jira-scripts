e006fcd36b047ab1365e8747ce83a9cd3c4ab875
Haswell33 <31355807+Haswell33@users.noreply.github.com>
2022-06-14 19:07:06 +0200

    Update getters.groovy

--- getters.groovy
+++ getters.groovy
@@ -1,7 +1,13 @@
+import javax.servlet.http.HttpServletRequest
+import javax.ws.rs.core.MultivaluedMap
+import javax.ws.rs.core.Response
+import groovy.json.JsonBuilder
+import groovy.transform.BaseScript
+import groovy.transform.Field
 import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.bc.JiraServiceContextImpl
-import com.atlassian.jira.jql.parser.JqlQueryParser
 import com.atlassian.jira.bc.issue.search.SearchService
+import com.atlassian.jira.jql.parser.JqlQueryParser
 import com.atlassian.jira.web.bean.PagerFilter
 import com.atlassian.sal.api.ApplicationProperties
 import com.atlassian.sal.api.UrlMode
@@ -11,12 +17,6 @@ import org.apache.commons.httpclient.auth.AuthScope
 import org.apache.commons.httpclient.UsernamePasswordCredentials
 import org.apache.commons.httpclient.methods.*
 import org.apache.commons.httpclient.HttpClient
-import javax.servlet.http.HttpServletRequest
-import javax.ws.rs.core.MultivaluedMap
-import javax.ws.rs.core.Response
-import groovy.json.JsonBuilder
-import groovy.transform.BaseScript
-import groovy.transform.Field
 
 @BaseScript CustomEndpointDelegate delegate
 
@@ -30,8 +30,16 @@ def avatarService = ComponentAccessor.getAvatarService()
 def applicationProperties = ScriptRunnerImpl.getOsgiService(ApplicationProperties)
 def baseUrl = applicationProperties.getBaseUrl(UrlMode.ABSOLUTE)
 
-def API_USERNAME = '123'
-def API_PASS = '123'
+class dataObj {
+    String id
+    String text
+    String imgSrc
+}
+
+class dataObjAlt {
+    String id
+    String text
+}
 
 @Field Collection departmentsList = ['department1', 'department2', 'department2']
 def managerGroup = 'managers-group'
@@ -64,7 +72,8 @@ getDirector(httpMethod: "GET", groups: ['jira-api-users']) { MultivaluedMap quer
         log.warn("department of selected user does not have director, president ${president} returned")
         director = president
     }
-    return Response.ok(new JsonBuilder(director: director).toString()).build()
+    //return Response.ok(new JsonBuilder(director: director).toString()).build()
+    return Response.ok(director).build()
 }
 
 getManager(httpMethod: "GET", groups: ['jira-api-users']) { MultivaluedMap queryParams, body, HttpServletRequest request ->
@@ -90,18 +99,7 @@ getManager(httpMethod: "GET", groups: ['jira-api-users']) { MultivaluedMap query
             }
         }
     }
-    if (manager == null){ // if manager not found use director as manager
-        def client = new HttpClient();
-        def method = new GetMethod("${baseUrl}/rest/scriptrunner/latest/custom/getDirector?username=${queryParams.getFirst('username') as String}")
-        def credentials = new UsernamePasswordCredentials(API_USERNAME, API_PASS)
-        client.getParams().setAuthenticationPreemptive(true)
-        client.getState().setCredentials(AuthScope.ANY, credentials)
-        client.executeMethod(method)
-        manager = method.getResponseBodyAsString()
-        method.releaseConnection()
-        log.warn("team of selected user does not have manager, director as manager '${director}' returned")
-    }
-    return Response.ok(new JsonBuilder(manager: manager).toString()).build()
+    return Response.ok(manager).build()
 }
 
 def groupIsTeam(group){
@@ -114,35 +112,28 @@ def groupIsTeam(group){
     return false
 }
 
-class Component {
-    String id
-    String text
-}
-
-getAssetComponents(httpMethod: 'GET', groups: ['jira-core-users', 'jira-software-users', 'jira-servicedesk-users']) { MultivaluedMap queryParams, body ->
+getProjectComponents(httpMethod: 'GET', groups: ['jira-core-users', 'jira-software-users', 'jira-servicedesk-users']) { MultivaluedMap queryParams, body ->
+    def projectManager = ComponentAccessor.getProjectManager()
+    
     def type = queryParams.getFirst('type') as String
     if(type == null){
         log.error('description of components not provided')
         return Response.status(400).build()
     }
-    def project = issueManager.getIssueObject('ASSET-1').getProjectObject()  // to load available options related to project
+    def project = projectManager.getProjectObjByKey(queryParams.getFirst('projectKey') as String)  // to load available options related to project
     def componentsUnfiltered = project.getComponents()
     def componentList = []
     componentsUnfiltered.each { component ->
-        if ((component['description'] as String).contains(type) || ((component['description'] as String).toLowerCase()).contains(type))
-            componentList.add(new Component(id: component['name'] as String, text: component['name'] as String))
+        log.warn(component.name)
+        if (component['description'] != null)
+            if ((component['description'] as String).contains(type) || ((component['description'] as String).toLowerCase()).contains(type))
+                componentList.add(new dataObjAlt(id: component['name'] as String, text: component['name'] as String))
     }
     return Response.ok(componentList).build()
 }
 
-class User {
-    String id
-    String text
-    String imgSrc
-}
-
 getActiveUsers(httpMethod: 'GET', groups: ['jira-core-users', 'jira-software-users', 'jira-servicedesk-users']) { MultivaluedMap queryParams, body ->
-    def accessToken = 'dupa'
+    def accessToken = ''
     def providedToken = queryParams.getFirst('accessToken') as String
     if (providedToken != accessToken){
         log.error("token ${providedToken} is not valid")
@@ -154,21 +145,15 @@ getActiveUsers(httpMethod: 'GET', groups: ['jira-core-users', 'jira-software-use
     def usersList = []
     for (user in users)
         if(user.isActive())
-            usersList.add(new User(id: user.name, text: user.displayName, imgSrc: avatarService.getAvatarURL(loggedUser, user) as String))
+            usersList.add(new dataObj(id: user.name, text: user.displayName, imgSrc: avatarService.getAvatarURL(loggedUser, user) as String))
     return Response.ok(new JsonBuilder(usersList).toString()).build()
 }
 
-class Access {
-    String id
-    String text
-    String imgSrc
-}
-
 getPersonAccesses(httpMethod: 'GET', groups: ['jira-core-users', 'jira-software-users', 'jira-servicedesk-users']) { MultivaluedMap queryParams, body ->
-	def accessToken = 'dupa'
+	def accessToken = ''
     def providedToken = queryParams.getFirst('accessToken') as String
     if (providedToken != accessToken){
-        log.error('token ${providedToken} is not valid')
+        log.error("token ${providedToken} is not valid")
         return Response.status(400).build()
     }
     def users = (queryParams.getFirst('username') as String)
@@ -183,11 +168,71 @@ getPersonAccesses(httpMethod: 'GET', groups: ['jira-core-users', 'jira-software-
         results.each { issue ->
             issue.getIssueType().getAvatar()
             if (usernames.size() > 1)
-            	accessList.add(new Access(id: "${issue.key}", text: "${issue.key} ${issue.summary} (${user.displayName})", imgSrc: issue.getIssueType().getIconUrl()))
+            	accessList.add(new dataObj(id: "${issue.key}", text: "${issue.key} ${issue.summary} (${user.displayName})", imgSrc: issue.getIssueType().getIconUrl()))
             else
-                accessList.add(new Access(id: "${issue.key}", text: "${issue.key} ${issue.summary}", imgSrc: issue.getIssueType().getIconUrl()))
+                accessList.add(new dataObj(id: "${issue.key}", text: "${issue.key} ${issue.summary}", imgSrc: issue.getIssueType().getIconUrl()))
         }
     }
-
     return Response.ok(new JsonBuilder(accessList).toString()).build()
+}
+
+getPersonAsset(httpMethod: 'GET', groups: ['jira-core-users', 'jira-software-users', 'jira-servicedesk-users']) { MultivaluedMap queryParams, body ->
+    def user = userManager.getUserByName((queryParams.getFirst('username') as String)) 
+    def query = jqlQueryParser.parseQuery("issuetype = 'Person' AND 'User' = $user.name")
+    log.warn(query)
+    def results = (searchService.search(userManager.getUserByName('admin'), query, PagerFilter.getUnlimitedFilter()))
+    if (results.getTotal() > 0){
+        //def issueList = []
+        def currIssue
+        results.getResults().each { issue ->
+            currIssue = issue.key
+            //issueList.add(issue.key)
+            log.warn("Person asset for ${user.name} already exists")
+        }
+        //return Response.status(409).entity(new JsonBuilder(currIssue).toString()).build()
+        return Response.status(409).entity(currIssue).build()
+    }
+    else
+        return Response.ok().build()
+}
+
+getDevicesOnStock(httpMethod: 'GET', groups: ['jira-core-users', 'jira-software-users', 'jira-servicedesk-users']) { MultivaluedMap queryParams, body ->
+    def customFieldManager = ComponentAccessor.getCustomFieldManager()
+
+	def accessToken = ''
+    def providedToken = queryParams.getFirst('accessToken') as String
+    if (providedToken != accessToken){
+        log.error("token ${providedToken} is not valid")
+        return Response.status(400).build()
+    }
+    def serialNumberField = customFieldManager.getCustomFieldObject(11504)
+    def deviceList = []
+    def query = jqlQueryParser.parseQuery("issuetype = 'Device' AND Status = 'On stock'")
+    log.warn(query)
+    def results = (searchService.search(userManager.getUserByName('admin'), query, PagerFilter.getUnlimitedFilter())).getResults()
+    results.each { issue ->
+        log.warn(issue.getCustomFieldValue(serialNumberField))
+        if (issue.getCustomFieldValue(serialNumberField) == null)
+            deviceList.add(new dataObj(id: "${issue.key}", text: "${issue.key} ${issue.summary} (no S/N)", imgSrc: issue.getIssueType().getIconUrl()))
+        else
+            deviceList.add(new dataObj(id: "${issue.key}", text: "${issue.key} ${issue.summary} (${issue.getCustomFieldValue(serialNumberField)})", imgSrc: issue.getIssueType().getIconUrl()))            
+    }
+    return Response.ok(new JsonBuilder(deviceList).toString()).build()
+}
+
+getCustomFieldOptionsById(httpMethod: 'GET', groups: ['jira-core-users', 'jira-software-users', 'jira-servicedesk-users']) { MultivaluedMap queryParams, body ->
+    def optionsManager = ComponentAccessor.getOptionsManager()
+    def customFieldManager = ComponentAccessor.getCustomFieldManager()
+
+    def field = customFieldManager.getCustomFieldObject(queryParams.getFirst('id') as Long)
+    def issue = issueManager.getIssueObject(queryParams.getFirst('contextIssue') as String)
+    def accessToken = ''
+    def providedToken = queryParams.getFirst('accessToken') as String
+    def optionList = []
+    def availableOptions = optionsManager.getOptions(field.getRelevantConfig(issue))
+    log.warn(availableOptions)
+    availableOptions.each { option ->
+        optionList.add(new dataObjAlt(id: option.getValue(), text: option.getValue()))
+    }
+    return Response.ok(new JsonBuilder(optionList).toString()).build()
 }
