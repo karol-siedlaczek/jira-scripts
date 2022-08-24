369b463df5e16cc8f55b9e301f3c81b9290d255c
Haswell33 <31355807+Haswell33@users.noreply.github.com>
2022-08-24 13:51:02 +0200

    Update getRequest.groovy

--- requests/getRequest.groovy
+++ requests/getRequest.groovy
@@ -2,9 +2,10 @@ import org.apache.commons.httpclient.HttpClient
 import org.apache.commons.httpclient.methods.*
   
 def client = new HttpClient();
-def method = new GetMethod("https://example.com)
+def method = new GetMethod("https://example.com")
 def credentials = new UsernamePasswordCredentials('username', 'password')
 client.getParams().setAuthenticationPreemptive(true)
 client.getState().setCredentials(AuthScope.ANY, credentials)
 client.executeMethod(method)
+method.releaseConnection()
 return method.getResponseBodyAsString()
